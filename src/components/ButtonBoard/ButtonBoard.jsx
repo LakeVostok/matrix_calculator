dfae1912dd7029297b1bf3b2e784825095b8a279
Nikita Kulikov <cola100@yandex.ru>
2017-09-20 03:15:20 +0500

    added radiobuttons

--- src/components/ButtonBoard/ButtonBoard.jsx
+++ src/components/ButtonBoard/ButtonBoard.jsx
@@ -9,6 +9,9 @@ export default class ButtonBoard extends Component {
                 <button onClick={this.props.clear}>Очистить матрицы</button>
                 <br />
                 <button onClick={this.props.swap}>Поменять матрицы местами</button>
+                <br />
+                <input onChange={this.props.selectActive} value="A" type="radio" name="choose" />Матрица А
+                <input onChange={this.props.selectActive} value="B" type="radio" name="choose" />Матрица В
             </div>
         );
     }
