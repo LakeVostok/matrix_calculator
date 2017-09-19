fb7c74f61d4ab61caa7e7f2867f8de1a1656f165
Nikita Kulikov <cola100@yandex.ru>
2017-09-20 03:23:58 +0500

    buttons for control rows and columns

--- src/components/ButtonBoard/ButtonBoard.jsx
+++ src/components/ButtonBoard/ButtonBoard.jsx
@@ -12,6 +12,12 @@ export default class ButtonBoard extends Component {
                 <br />
                 <input onChange={this.props.selectActive} value="A" type="radio" name="choose" />Матрица А
                 <input onChange={this.props.selectActive} value="B" type="radio" name="choose" />Матрица В
+                <br />
+                <button onClick={this.props.addRow}>Добавить</button>
+                <button onClick={this.props.removeRow}>Удалить</button> Строку
+                <br />
+                <button onClick={this.props.addColumn}>Добавить</button>
+                <button onClick={this.props.removeColumn}>Удалить</button> Столбец
             </div>
         );
     }
