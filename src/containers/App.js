f8710a986338db378f126c30737eaa7161c740ba
Nikita Kulikov <cola100@yandex.ru>
2017-09-20 01:02:11 +0500

    added MatrixBoard to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
+import MatrixBoard from "../components/MatrixBoard"
+
+function Storage(rows, columns) {
+    let storage = [];
+
+    for(let i = 0; i < rows; i++) {
+        storage.push([]);
+
+        for(let j = 0; j < columns; j++) {
+            storage[i].push("")
+        }
+    }
+
+    return storage;
+}
 
 export default class App extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            A: Storage(4, 2),
+            B: Storage(2, 3),
+            Result: Storage(4,3)
+        }
     }
 
     render() {
         return (
             <div>
-                <h1>It works!</h1>
+                <MatrixBoard
+                    Result={this.state.Result}
+                    A={this.state.A}
+                    B={this.state.B}
+                />
             </div>  
         );
     }
