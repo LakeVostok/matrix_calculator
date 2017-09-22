1d19c3d02d271c77f752a1f3900cef54fbd624d2
Nikita Kulikov <cola100@yandex.ru>
2017-09-22 16:30:56 +0500

    added identaton between matrices

--- src/components/MatrixBoard/MatrixBoard.jsx
+++ src/components/MatrixBoard/MatrixBoard.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
-import Matrix from "../Matrix"
+import Matrix from "../Matrix";
+import Gapped from "../Gapped";
+
+import "./MatrixBoard.scss";
 
 export default class MatrixBoard extends Component {
     render() {
         let { A, B, Result, onChange } = this.props;
         return (
-            <div>
-                <div>
-                    <Matrix
-                        data={Result}
-                        placeholder="c"
-                        disabled
-                    />
-                    <Matrix
-                        data={A}
-                        placeholder="a"
-                        labelRight="A"
-                        onChange={(row, column, value) => onChange("A", row, column, value)}
-                    />
-                </div>
-                <div>
+            <div className="matrix-board">
+                <Gapped gap={40} vertical>
+                    <Gapped gap={40}>
+                        <Matrix
+                            data={Result}
+                            placeholder="c"
+                            disabled
+                        />
+                        <Matrix
+                            data={A}
+                            placeholder="a"
+                            labelRight="A"
+                            onChange={(row, column, value) => onChange("A", row, column, value)}
+                        />
+                    </Gapped>
                     <Matrix
                         data={B}
                         placeholder="b"
                         labelBottom="B"
                         onChange={(row, column, value) => onChange("B", row, column, value)}
                     />
-                </div>
+                </Gapped>
             </div>  
         );
     }
