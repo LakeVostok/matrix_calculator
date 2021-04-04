8d6919ccad1f43706e9ab348b04b96fd9d4b6360
LakeVostok <lakevostok@yandex.ru>
2021-04-04 18:27:41 +0500

    added the new development environment

---
+++ .eslintrc.js
@@ -0,0 +1,105 @@
+module.exports = {
+	extends: [
+		"react-app",
+		"react-app/jest",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:import/typescript",
+	],
+	plugins: [
+		"import",
+	],
+	rules: {
+		"linebreak-style": [
+			"error",
+			"windows",
+		],
+		"eol-last": [
+			"error",
+			"always",
+		],
+		"no-multiple-empty-lines": [
+			"error",
+			{
+				max: 1,
+			},
+		],
+		"import/newline-after-import": [
+			"error",
+			{
+				count: 1,
+			},
+		],
+		quotes: [
+			"error",
+			"double",
+			{
+				allowTemplateLiterals: true,
+			},
+		],
+		"quote-props": ["error", "as-needed",],
+		semi: [
+			"error",
+			"always",
+		],
+		"comma-dangle": [
+			"error",
+			{
+				arrays: "always",
+				objects: "always",
+			},
+		],
+		indent: [
+			"error",
+			"tab",
+		],
+		"object-curly-spacing": [
+			"error",
+			"always",
+		],
+		"react/jsx-curly-spacing": [
+			"error",
+			"always",
+		],
+		"react/jsx-indent": [
+			"error",
+			"tab",
+		],
+		"import/order": [
+			"error",
+			{
+				groups: [
+					"builtin", 
+					"external", 
+					"parent", 
+					"internal", 
+					"object",
+					"sibling", 
+					"index", 
+				],
+				"newlines-between": "always",
+			},
+		],
+	},
+	overrides: [
+		{
+			files: [
+				"**/*.ts?(x)",
+			],
+			rules: {
+				"@typescript-eslint/indent": [
+					"error",
+					"tab",
+				],
+			},
+		},
+		{
+			files: [
+				"**/*.stories.*",
+			],
+			rules: {
+				"import/no-anonymous-default-export": "off",
+			},
+		},
+	],
+};
